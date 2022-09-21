83dea0d4defeed423dd5a715c6d958f9f3928f0c
Atem Akuei <atem.akuei@gmail.com>
2022-09-21 11:33:37 -0500

    Updated

--- Light.java
+++ Light.java
@@ -5,11 +5,11 @@ public class Light {
   Chair chair;
   double comfort;
 
-  public void print(String message){
+  public void print(String message) {
     System.out.print(message);
   }
 
-  public void printLn(String message){
+  public void printLn(String message) {
     System.out.println(message);
   }
 
@@ -20,19 +20,19 @@ public class Light {
     isOn = false;
   }
 
-  public Light(String type){
+  public Light(String type) {
     this();
     this.type = type;
   }
-  
-  public Light(int lumens, int watts, String type, boolean isOn){
+
+  public Light(int lumens, int watts, String type, boolean isOn) {
     this.lumens = lumens;
     this.watts = watts;
     this.type = type;
     this.isOn = isOn;
   }
 
-  public Light(Chair chair, int lumens, int watts, String type, boolean isOn, double comfort){
+  public Light(Chair chair, int lumens, int watts, String type, boolean isOn, double comfort) {
     this.chair = chair;
     this.lumens = lumens;
     this.watts = watts;
@@ -41,61 +41,62 @@ public class Light {
     this.comfort = comfort;
   }
 
-  public void turnOn(){
+  public void turnOn() {
     isOn = true;
   }
 
-  public void turnOff(){
+  public void turnOff() {
     isOn = false;
   }
 
-  public int getLumens(){
+  public int getLumens() {
     return lumens;
   }
 
-  public int getWatts(){
+  public int getWatts() {
     return watts;
   }
 
-  public String getType(){
+  public String getType() {
     return type;
   }
 
-  public boolean isOn(){
+  public boolean isOn() {
     return isOn;
   }
 
-  public void setLumens(int lumens){
+  public void setLumens(int lumens) {
     this.lumens = lumens;
   }
 
-  public void setWatts(int watts){
+  public void setWatts(int watts) {
     this.watts = watts;
   }
 
-  public void changeType(String type){
+  public void changeType(String type) {
     this.type = type;
   }
 
-  public void shine(){
+  public void shine() {
     isOn = true;
   }
 
-  public void breakLight(){
+  public void breakLight() {
     isOn = true;
     type = "Broken";
   }
 
-  public void uninstall(){
+  public void uninstall() {
     isOn = false;
   }
 
-  public void sit(){
-    printLn("You sit in your " + chair.getColor() + " chair light. On a scale from 1-10 on how comfortable it is, it's a " + comfort + ". It's " + lumens + " lumens bright. Now you can enjoy a book!");
+  public void sit() {
+    printLn("You sit in your " + chair + " chair light. On a scale from 1-10 on how comfortable it is, it's a "
+        + comfort + ". It's " + lumens + " lumens bright. Now you can enjoy a book!");
   }
-  
-  public String toString(){
-    return("Lumens: " + lumens + " | Watts: " + watts + " | Type: " + type + " | Is it on? " + isOn);
+
+  public String toString() {
+    return ("Lumens: " + lumens + " | Watts: " + watts + " | Type: " + type + " | Is it on? " + isOn);
   }
-  
+
 }
