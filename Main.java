04f7faa6f64ed6a2137caa09ceab99f8c6ebca59
Atem Akuei <atem.akuei@gmail.com>
2022-09-21 10:09:53 -0500

    Added chair light

--- Main.java
+++ Main.java
@@ -14,6 +14,11 @@ class Main {
         Light l2 = new Light(20, 20, "LED", false);
         l.turnOn();
 
+        Chair c1 = new Chair(10, 10, "Red");
+        Light s1 = new Light(c1, 100, 100, "Chair Light", true, 8.5);
+
+        s1.sit();
+
         System.out.println(l);
         System.out.println(l2);
       
